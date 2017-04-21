ecaebba78880d14db3fa8b0c1b01b2c4a65c3973
mitutee <volman1285@gmail.com>
2017-04-21 09:58:27 +0300

    Init

---
+++ GarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            CreateJunior("IDontUseGC");
+            //System.GC.Collect(); // Without this we dont collect our Junior
+
+            for (int i = 0; i < 99999; i++) { // GC has came here!!!
+                new Junior(i.ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        static void CreateJunior(string name)
+        {
+            new Junior(name);           
+        }
+    }
+
+    class Junior : System.Object
+    {
+        public string Name { get; set; }
+        public Junior(string name)
+        {
+            Name = name;
+            Console.WriteLine("I am hired!, {0}", Name);
+        }
+        ~Junior()
+        {
+            Console.WriteLine("I am fired =(, {0}", Name);
+        }
+    }
+}
